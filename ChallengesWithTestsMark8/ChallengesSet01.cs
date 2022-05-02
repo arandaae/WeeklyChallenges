d5c432c1e5ff1c18912b8872af52b09ab9dd8677
Armando Aranda <arandaae19@gmail.com>
2022-05-01 23:13:07 -0500

    First commit, completed challenge set 01

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,38 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            
+            var answer = minuend - subtrahend;
+            return answer;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            var answer = number1 + number2;
+            return answer;
         }
 
         public int GetSmallestNumber(int number1, int number2)
-        {
-            throw new NotImplementedException();
+        {           
+                return Math.Min(number1, number2);
+                
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            var answer =factor1 * factor2;
+            return answer;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            return nameOfPerson;
+             
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "Hey";
         }
     }
 }
